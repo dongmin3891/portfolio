2fc68369dbc49fcc2d2b1f71da8d05cb4c659e18
KimDongMin <rlaehdals753@naver.com>
2020-05-18 17:29:07 +0900

    JS: Home Transparent

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ document.addEventListener("scroll", () => {
 
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
-  console.log(event);
   const target = event.target;
   const link = target.dataset.link;
   if (link === null) {
@@ -34,3 +33,9 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+// home에서 벗어날 시 home 점점 투명
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
